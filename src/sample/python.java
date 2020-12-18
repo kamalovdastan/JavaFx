b8f0949b6c968bd3ac4d8b6790413c5bece907d7
kamalovdastan <55980298+kamalovdastan@users.noreply.github.com>
2020-12-19 05:59:29 +0600

    Add files via upload

---
+++ src/sample/python.java
@@ -0,0 +1,47 @@
+package pythonquestion;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.text.Text;
+
+public class python {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button Second;
+
+    @FXML
+    private RadioButton radio_btn_1;
+
+    @FXML
+    private ToggleGroup answers;
+
+    @FXML
+    private Text question_text;
+
+    @FXML
+    private RadioButton radio_btn_2;
+
+    @FXML
+    private RadioButton radio_btn_3;
+
+    @FXML
+    private RadioButton radio_btn_4;
+
+    @FXML
+    private Button answerBtn;
+
+    @FXML
+    void initialize() {
+
+    }
+}
